951b033eab62159ebdca7ca838f698797b3eb9bd
Sergey Efremov <neserge@gmail.com>
2017-01-29 13:59:57 +0300

    Another emacs branch.

---
+++ emacs.d/elpa/pyenv-mode-0.1.0/pyenv-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "pyenv-mode" "0.1.0" "Integrate pyenv with python-mode" '((pythonic "0.1.0")) :url "https://github.com/proofit404/pyenv-mode")
---
+++ emacs.d/elpa/pyenv-mode-auto-20160122.2341/pyenv-mode-auto-autoloads.el
@@ -0,0 +1,16 @@
+;;; pyenv-mode-auto-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("pyenv-mode-auto.el") (22493 33599 744524
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; pyenv-mode-auto-autoloads.el ends here
